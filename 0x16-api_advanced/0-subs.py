10a2b5b2e4543d8962c131394daac4e6d8b08fca
CrimsonPhoenix666 <136012873+CrimsonPhoenix666@users.noreply.github.com>
2024-03-07 13:59:19 +0100

    Update 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 """
-A recursive function that queries the Reddit API for the total number of subscribers for a given subreddit.
+A recursive function that querries reddit API for total number of subsribers
 """
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """ Queries the Reddit API """
+    """ Queries to Reddit API """
     u_agent = 'Mozilla/5.0'
 
     headers = {
@@ -15,23 +15,11 @@ def number_of_subscribers(subreddit):
 
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     res = requests.get(url, headers=headers, allow_redirects=False)
-    print(f"URL: {url}")
-    print(f"Status Code: {res.status_code}")
-
     if res.status_code != 200:
         return 0
-
     dic = res.json()
-    print(f"JSON Response: {dic}")
-
     if 'data' not in dic:
         return 0
     if 'subscribers' not in dic.get('data'):
         return 0
-
-    return dic['data']['subscribers']
-
-# Example usage:
-subreddit_name = "programming"
-result = number_of_subscribers(subreddit_name)
-print(f"Subscribers for {subreddit_name}: {result}")
+    return res.json()['data']['subscribers']
