6a4c8b8dba5fbf6af9758042847861f1bf38bcb7
CrimsonPhoenix666 <136012873+CrimsonPhoenix666@users.noreply.github.com>
2024-03-07 14:07:28 +0100

    Update 1-top_ten.py

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,31 +1,56 @@
 #!/usr/bin/python3
 """
-Queries Reddit API and prints the titles of the first 10 posts
+Queries Reddit API and prints the titles of the first 10 posts using recursion
 """
 import requests
 
 
-def top_ten(subreddit):
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
+def add_title(hot_list, hot_posts):
+    """ Adds item into a list """
+    if len(hot_posts) == 0:
+        return
+    hot_list.append(hot_posts[0]['data']['title'])
+    hot_posts.pop(0)
+    add_title(hot_list, hot_posts)
+
+
+def recurse(subreddit, hot_list=[], after=None):
+    """ Queries Reddit API recursively """
+    u_agent = 'Mozilla/5.0'
     headers = {
-        'User-Agent': 'Mozilla/5.0'
+        'User-Agent': u_agent
     }
-    response = requests.get(url, headers=headers, allow_redirects=False)
+
+    params = {
+        'after': after
+    }
+
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
+    response = requests.get(url, headers=headers, params=params, allow_redirects=False)
 
     if response.status_code != 200:
-        print(None)
-        return
+        return None
 
-    try:
-        data = response.json()
-        # Check if 'data' key is present and 'children' is not empty
-        if 'data' in data and 'children' in data['data'] and data['data']['children']:
-            for post in data['data']['children']:
-                print(post['data']['title'])
-        else:
-            print(None)
-    except (KeyError, ValueError):
+    data = response.json()
+    hot_posts = data['data']['children']
+    add_title(hot_list, hot_posts)
+    after = data['data']['after']
+
+    if not after or len(hot_list) >= 10:
+        return hot_list
+
+    return recurse(subreddit, hot_list=hot_list, after=after)
+
+
+def top_ten(subreddit):
+    """Top ten function using recursion"""
+    hot_list = recurse(subreddit)
+    
+    if hot_list is None or len(hot_list) == 0:
         print(None)
+    else:
+        for title in hot_list[:10]:
+            print(title)
 
 if __name__ == "__main__":
     import sys
@@ -34,4 +59,3 @@ if __name__ == "__main__":
         print("Please pass an argument for the subreddit to search.")
     else:
         top_ten(sys.argv[1])
-
